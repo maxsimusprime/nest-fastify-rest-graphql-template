c1ce169fd9f5516c502e00cd5128e2e189cfcffc
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2024-05-31 19:02:03 +0300

    chore: switch app to fastify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 const PORT = Number(process.env.PORT);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   await app.listen(PORT);
 }
